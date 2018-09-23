975e4998fb800630437b1f9068f1896895168d04
balajikannan4 <43474126+balajikannan4@users.noreply.github.com>
2018-09-23 23:27:53 +0530

    Add files via upload

---
+++ ex10.rb
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabbed in" # declare variable tabby_cat, \t used for 'TAB'
+persian_cat = "I'm split\non a line" # declare variable persian_cat, \n used for new line
+backslash_cat = "I'm \\ a \\ cat" # declare variable backslash_cat, \\ used for '\'
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+""" # declare variable fat_cat, """ used for
+
+puts tabby_cat # display and call the value of tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
---
+++ ex9.rb
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun" # declare the variable 'days'
+
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # declare the months, '\n' for new line
+
+puts "Here are the days: #{days}" # call the days value
+puts "Here are the months: #{months}" # call the month value
+
+puts %q{ 
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
+# we use %q for write a paragraph
